db788e067ca487e02ec009b4680953004e4e5568
Vili Kinnunen <vili.kinnunen1@gmail.com>
2019-09-15 14:39:27 +0300

    Move recipes to card view

--- app/src/main/java/in/vilik/wattoeat/MainActivity.kt
+++ app/src/main/java/in/vilik/wattoeat/MainActivity.kt
@@ -1,7 +1,7 @@
 package `in`.vilik.wattoeat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/in/vilik/wattoeat/recipelist/RecipeListViewModel.kt
@@ -0,0 +1,24 @@
+package `in`.vilik.wattoeat.recipelist
+
+import `in`.vilik.wattoeat.Recipe
+import `in`.vilik.wattoeat.repository.RecipeRepository
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class RecipeListViewModel : ViewModel() {
+
+    private val recipeRepository = RecipeRepository()
+
+    private val _recipes : MutableLiveData<List<Recipe>> = MutableLiveData(emptyList())
+
+    val recipes: LiveData<List<Recipe>> = _recipes
+
+    init {
+        viewModelScope.launch {
+            _recipes.value = recipeRepository.findAllRecipes()
+        }
+    }
+}
